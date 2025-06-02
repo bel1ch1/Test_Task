d5ddae9c52b6c0685a4be1271a2d5beda020bd6c
bel1ch1 <andreyzv5555@gmail.com>
2025-06-02 18:53:47 +0300

    People counter

--- pacages/model_processing.py
+++ pacages/model_processing.py
@@ -16,8 +16,25 @@ def model_processing(input_path, output_path, conf_threshold=0.5):
 
     for frame in video.get_frames():
         boxes = detector.detect(frame)
+        counter = 0
         for box in boxes:
+            counter += 1
             x1, y1, x2, y2 = map(int, box)
             cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
+
+        text = f'Detected: {counter}'
+        font = cv2.FONT_ITALIC
+        font_scale = 2
+        color = (0, 0, 255)
+        thickness = 3
+        margin = 20
+
+        (text_width, text_height), _ = cv2.getTextSize(text, font, font_scale, thickness)
+
+        x_pos = frame.shape[1] - text_width - margin
+        y_pos = text_height + margin
+
+        cv2.putText(frame, text, (x_pos, y_pos), font, font_scale, color, thickness, cv2.LINE_AA)
+
         writer.write(frame)
     writer.release()
